981e27cb06254ca200ecece9ac40741612e3c5f9
TstCake <160360135+TstCake@users.noreply.github.com>
2024-02-18 01:15:22 +0400

    Update MyProject-HTML5-Shipping.data.js

--- MyProject-HTML5-Shipping.data.js
+++ MyProject-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Users/ilyak/OneDrive/Документы/Unreal Projects/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/uzyacxvy0xp9gx7j14cez/MyProject-HTML5-Shipping.data?rlkey=7fyau2b39zk187nf7g11jllua&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/uzyacxvy0xp9gx7j14cez/MyProject-HTML5-Shipping.data?rlkey=7fyau2b39zk187nf7g11jllua&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
@@ -149,10 +149,10 @@ Module['FS_createPath']('/MyProject/Content', 'Paks', true, true);
           for (var i = 0; i < files.length; ++i) {
             DataRequest.prototype.requests[files[i].filename].onload();
           }
-              Module['removeRunDependency']('datafile_C:/Users/ilyak/OneDrive/Документы/Unreal Projects/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data');
+              Module['removeRunDependency']('datafile_C:/Users/ilyak/OneDrive/Г„Г®ГЄГіГ¬ГҐГ­ГІГ»/Unreal Projects/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data');
 
     };
-    Module['addRunDependency']('datafile_C:/Users/ilyak/OneDrive/Документы/Unreal Projects/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data');
+    Module['addRunDependency']('datafile_C:/Users/ilyak/OneDrive/Г„Г®ГЄГіГ¬ГҐГ­ГІГ»/Unreal Projects/MyProject/Binaries/HTML5/MyProject-HTML5-Shipping.data');
   
     if (!Module.preloadResults) Module.preloadResults = {};
   
